b586aa7ad092085953c6865d2566faafb9b5ec8f
Alexander Somov <somov.a.l@gmail.com>
2015-02-28 15:07:05 +0300

    First working test

--- addrbook/test_address.py
+++ addrbook/test_address.py
@@ -6,4 +6,13 @@ __author__ = 'somov'
 
 class TestAddress(TestCase):
     def test_match(self):
-        pass
+        addr = obj.Address('Russia', 'Moscow', 'Tolstogo', '16')
+
+        request = ['Russia']
+        self.assertTrue(addr.match(request))
+
+        request = ['Russia', 'Moscow']
+        self.assertTrue(addr.match(request))
+
+        request = ['Russia', 'Moscow', 'Tolstogo']
+        self.assertTrue(addr.match(request))
